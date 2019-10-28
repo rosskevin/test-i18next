39ce83da15765ff34f3d354fc523408f12cc9017
Kevin Ross <kevin.ross@alienfast.com>
2019-10-28 14:54:41 -0500

    esmoduleinterop: true working

--- src/test-helpers/setupEnzyme.ts
+++ src/test-helpers/setupEnzyme.ts
@@ -1,6 +1,6 @@
 import 'jest-enzyme'
 import { configure } from 'enzyme'
-import * as Adapter from 'enzyme-adapter-react-16'
+import Adapter from 'enzyme-adapter-react-16'
 // import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() })
